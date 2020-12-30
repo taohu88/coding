1eb56ad857e5e463b149b67deaf2af3adee734c3
Tao Hu <tahu@microsoft.com>
2020-12-30 15:06:28 -0800

    work on sum problem

---
+++ src/SumProblem/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SumProblem
+{
+    class Program
+    {
+
+        static List<Tuple<T, T>> FindSum<T>(List<T> aList, T target )
+        {
+            var r = new List<Tuple<T, T>>();
+
+            for (var i = 0; i < aList.Count; ++i)
+            {
+                for (var j = i+1; j < aList.Count; ++j)
+                {
+                    if (((dynamic)aList[i] + (dynamic)aList[j]) == target)
+                    {
+                        r.Add(new Tuple<T, T>(aList[i], aList[j]));
+                    }
+                }
+            }
+            return r;
+        }
+
+        static List<Tuple<T, T>> FastSum<T>(List<T> aList, T target)
+        {
+            var r = new List<Tuple<T, T>>();
+            var h = new Dictionary<T, T>();
+
+            foreach (var x in aList)
+            {
+                var y = (dynamic)target - (dynamic)x;
+                if (h.ContainsKey(y))
+                {
+                    r.Add(new Tuple<T, T>(y, x));
+                }
+                h.TryAdd(x, x);
+            }
+            return r;
+        }
+
+        static void Main(string[] args)
+        {
+            var numL = new List<int>();
+            var r = new Random();
+            for (var i = 0; i < 20; ++i)
+            {
+                numL.Add(r.Next(100));
+            }
+
+            foreach (var item in numL)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+
+            var target = r.Next(100);
+            Console.WriteLine($"Target is {target}");
+
+            var results = FastSum(numL, target);
+            foreach (var item in results)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+            //var testPerf = new TestArrayVsListPerformance();
+            //var t = testPerf.Test();
+            //Console.WriteLine(t);
+        }
+    }
+}
---
+++ src/SumProblem/TestArrayVsListPerformance.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace SumProblem
+{
+    class TestArrayVsListPerformance
+    {
+        private const int NumIterations = 10000;
+        private const int Length = 10000;
+
+        public string Test()
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            int sum;
+            var array = new int[Length];
+            var list = new List<int>(Length);
+            for (var i = 0; i < Length; ++i)
+            {
+                array[i] = i;
+                list.Add(i);
+            }
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (var it = 0; it < NumIterations; ++it)
+            {
+                sum = 0;
+                for (var i = 0; i < Length; ++i)
+                {
+                    sum += array[i];
+                }
+            }
+            var arrayReadTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (var it = 0; it < NumIterations; ++it)
+            {
+                for (var i = 0; i < Length; ++i)
+                {
+                    array[i] = i;
+                }
+            }
+            var arrayWriteTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (var it = 0; it < NumIterations; ++it)
+            {
+                sum = 0;
+                for (var i = 0; i < Length; ++i)
+                {
+                    sum += list[i];
+                }
+            }
+            var listReadTime = stopwatch.ElapsedMilliseconds;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (var it = 0; it < NumIterations; ++it)
+            {
+                for (var i = 0; i < Length; ++i)
+                {
+                    list[i] = i;
+                }
+            }
+            var listWriteTime = stopwatch.ElapsedMilliseconds;
+
+            string report =
+                "Test,Array Time,List Time\n"
+                + "Read," + arrayReadTime + "," + listReadTime + "\n"
+                + "Write," + arrayWriteTime + "," + listWriteTime;
+            return report;
+        }
+    }
+}
